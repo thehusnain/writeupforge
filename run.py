4efd1c9e66cb59afd7b90177143ff61385427254
Husnain <huxnain.cs@gmail.com>
2026-04-07 13:47:47 +0500

    fix: Add --version and -v flags to show tool version

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import platform
 import sys
 import click
+from version import CURRENT_VERSION
 
 
 def run():
@@ -8,6 +9,16 @@ def run():
     os_name = platform.system()
     force_gui = '--gui' in sys.argv
     force_cli = '--cli' in sys.argv
+    show_version = '--version' in sys.argv or '-v' in sys.argv
+
+    # Show version and exit
+    if show_version:
+        click.echo(click.style("\n" + "=" * 50, fg='blue', bold=True))
+        click.echo(click.style("WriteupForge - Professional Writeup Generator", fg='cyan', bold=True))
+        click.echo(click.style("=" * 50, fg='blue', bold=True))
+        click.echo(click.style(f"\nVersion: {CURRENT_VERSION}", fg='green', bold=True))
+        click.echo("")
+        sys.exit(0)
 
     if force_gui:
         from main_gui import run_gui
